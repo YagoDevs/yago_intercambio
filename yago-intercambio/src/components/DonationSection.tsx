fcc3412b4bc4316e1683fc5e98ed16ce5e37bac9
Yago Phelllipe <yago.lopes@sou.inteli.edu.br>
2025-07-08 11:38:38 -0300

    feat: valor

--- yago-intercambio/src/components/DonationSection.tsx
+++ yago-intercambio/src/components/DonationSection.tsx
@@ -8,7 +8,7 @@ const DonationSection = () => {
   const pixKey = "yagophellipe2012@live.com";
 
   // Dados de exemplo - você pode ajustar conforme nec  essário
-  const currentDonations = 2302.08; // Doações recebidas em BRL
+  const currentDonations = 2582.08; // Doações recebidas em BRL
 
   const expenses = [
     { item: "Passagem Aérea", amount: 6500 },
